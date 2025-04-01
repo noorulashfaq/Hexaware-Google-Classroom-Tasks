a90e99f0261db4e835f8d0e97e4e52d0bbedc54d
Noorul Ashfaq A <noorulashfaq786@gmail.com>
2025-04-01 19:38:48 +0530

    Card validation done

---
+++ Array/CardValidation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // string cardNumber = "2003113438647744";
+        Console.Write("Enter card number: ");
+        string cardNumber = Console.ReadLine();
+        Console.WriteLine(ValidateCard(cardNumber) ? "Valid Card Number" : "Invalid Card Number");
+    }
+
+    static bool ValidateCard(string cardNumber)
+    {
+        int sum = 0;
+        bool doubleDigit = true;
+
+        for (int i = cardNumber.Length - 1; i >= 0; i--)
+        {
+            int digit = cardNumber[i] - '0';
+            
+            if (doubleDigit)
+            {
+                digit *= 2;
+                if (digit > 9) 
+                    digit = (digit % 10) + 1;
+            }
+
+            sum += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return sum % 10 == 0;
+    }
+}
